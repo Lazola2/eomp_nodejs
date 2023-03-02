9bfeed1e92ee583e3db3e0c81852f08ff0f8a34d
Zweleth <zwebeko@gmail.com>
2023-03-02 09:13:00 +0200

    delete config file

---
+++ src/views/CRUDView.vue
@@ -0,0 +1,30 @@
+<template lang="">
+    <div>
+        <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">id</th>
+                <th scope="col">Model</th>
+                <th scope="col">Brand</th>
+                <th scope="col">Type</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              
+            </tbody>
+          </table>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
--- vue.config.js
+++
@@ -1,7 +0,0 @@
-const {defineConfig} = require('@vue/cli-service')
-module.exports = defineConfig({
-    transpileDependencies: true,
-    devServer: {
-        proxy: 'https://node-api-eomp.onrender.com/' 
-    }
-})
